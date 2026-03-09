92f48fcafb09f216706e1f4b592de3f5f34cab95
WenKai Shi <73520560+eliajhmauve@users.noreply.github.com>
2026-03-09 21:53:21 +0800

    fix: add basename to Router for GitHub Pages subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/birthday-decoder">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/compatibility" element={<Compatibility />} />
